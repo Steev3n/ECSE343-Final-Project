86652d963e3b011f1f7d7704ac0ece4b69023b48
SergeAllaham <sergelaham2004@gmail.com>
2026-03-30 17:38:17 -0400

    stuffs

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from circuit_simulator import CircuitSimulator
 import pickle
-import tqdm
 
 def plot_data(x_test, tpoints):
     # Create the figure and the first axis (for Volts)
@@ -48,12 +47,20 @@ def plot_data(x_test, tpoints):
     plt.title("Voltage and Current vs. Time")
     plt.tight_layout()
     plt.show()
-    
-def create_dataset(num_samples, amplitude, f, delta_t, T, noise) -> tuple[np.ndarray, np.ndarray]:
+
+"""
+inputs: number of samples, amplitude, frequency, delta_time, Period T, noise 
+
+
+"""
+
+
+
+def create_dataset(num_samples, amplitude, f, delta_t, T, noise):
     x = [] # To store the simulated transient responses (features)
     y = [] # To store the ground truth R and C values (labels)
 
-    for i in tqdm.tqdm(range(num_samples)):
+    for i in range(num_samples):
         # Randomly sample resistance (1 to 5k Ohms) and capacitance (0.1 to 10 microFarads)
         """ YOUR CODE HERE:
         R, C = ...
@@ -70,11 +77,13 @@ def create_dataset(num_samples, amplitude, f, delta_t, T, noise) -> tuple[np.nda
                 [np.log10(2500.0), np.log10(5e-6)]
             )
 
-        R = 10 ** create_dataset.lhs_log[i, 0]
-        C = 10 ** create_dataset.lhs_log[i, 1]
+        R = create_dataset.lhs_log[i, 0]
+        C = create_dataset.lhs_log[i, 1]
+
+        
 
         # Initialize the Modified Nodal Analysis (MNA) simulator with current parameters
-        mna = CircuitSimulator(amplitude, f, R, C)
+        mna = CircuitSimulator(amplitude, f, 10**R, 10**C)
         y.append([R, C])
 
         # Initialize state variables 
